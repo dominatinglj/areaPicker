d6706d4d04909a131caebd1f1dd2a18cb1ad5fdb
jun_liao <junliao3344@gmail.com>
2015-10-28 12:00:21 +0800

    Initial Commit

---
+++ AreaPickerTests/AreaPickerTests.m
@@ -0,0 +1,39 @@
+//
+//  AreaPickerTests.m
+//  AreaPickerTests
+//
+//  Created by 廖军 on 15/10/28.
+//  Copyright © 2015年 com.qiang. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AreaPickerTests : XCTestCase
+
+@end
+
+@implementation AreaPickerTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
